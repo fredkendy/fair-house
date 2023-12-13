f6709ae409d9799e64cf904bfa87838043880e4a
fredkendy <fredkendy9@gmail.com>
2023-12-13 16:18:01 -0300

    filtrar resultados da pesquisa

--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -12,6 +12,8 @@ export class HousingListComponent implements OnInit {
 
   //utilizando Input como decorador para locationList
   @Input() locationList: HousingLocation[] = [];
+  //A matriz de resultados representa os locais de residência que correspondem à pesquisa do usuário. 
+  results: HousingLocation[] = []
 
   constructor() { }
 
@@ -20,7 +22,18 @@ export class HousingListComponent implements OnInit {
 
   //Recebe parametro do botão no html
   searchHousingLocations(searchText: string) {
-    console.log(searchText);
+    //evitar a pesquisa na matriz se searchText estiver em branco
+    if (!searchText) return;
+    //usamos o filtro 
+    this.results = this.locationList.filter(
+      (location: HousingLocation) => location.city
+        //Todos os valores são comparados usando as versões em letras minúsculas das strings.
+        .toLowerCase()
+        //aceitamos somente valores que contenham o searchText
+        .includes(
+          searchText.toLowerCase()
+        )
+    );
   }
 
 }
